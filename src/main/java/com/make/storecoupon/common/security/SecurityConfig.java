375c57c14dd126d637318f226f3a6a2c7167b052
dangddoong <dangddoong@gmail.com>
2023-09-06 22:56:41 +0900

    Feat: JWT, SpringSecurity 기반으로 Customer, Mart의 AuthFilter를 추가하였습니다.

---
+++ src/main/java/com/make/storecoupon/common/security/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.make.storecoupon.common.security;
+
+import com.make.storecoupon.common.auth.jwt.JwtUtil;
+import com.make.storecoupon.common.security.filter.CustomerAuthFilter;
+import com.make.storecoupon.common.security.filter.MartAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig implements WebMvcConfigurer {
+
+  private final JwtUtil jwtUtil;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+  @Bean
+  protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    http.httpBasic().disable()
+        .csrf().disable()
+        .formLogin().disable();
+    http.formLogin().disable();
+    http.formLogin().disable();
+    http
+        .headers()
+        .xssProtection()
+        .and()
+        .contentSecurityPolicy("script-src 'self'");
+    http.sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 세션이 필요하면 생성하도록 셋팅
+
+    http.authorizeHttpRequests()
+        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+        .requestMatchers("/**").permitAll() // 현재 일단 다 열어놓음
+        .anyRequest().authenticated()
+        .and()
+        .addFilterBefore(new CustomerAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(new MartAuthFilter(jwtUtil), CustomerAuthFilter.class);
+
+    return http.build();
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD")
+        .exposedHeaders("Authorization");
+
+    registry.addMapping("/**")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD")
+        .exposedHeaders("Mart");
+  }
+
+}
