96df2f9c508afd0d6b8b2a44291cd43af72cbe3e
dangddoong <dangddoong@gmail.com>
2023-09-07 01:29:04 +0900

    Feat: Customer 회원가입시 @Valid기반 비밀번호 제약조건 추가

--- src/main/java/com/make/storecoupon/customer/dto/RegisterRequestDto.java
+++ src/main/java/com/make/storecoupon/customer/dto/RegisterRequestDto.java
@@ -1,6 +1,7 @@
 package com.make.storecoupon.customer.dto;
 
 import com.make.storecoupon.customer.entity.Customer;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,8 @@ import lombok.NoArgsConstructor;
 public class RegisterRequestDto {
 
   private String loginId;
+  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+      message = "비밀번호는 최소 문자 1개, 숫자 1개를 포함한 8자리 이상으로 설정해주세요.")
   private String password;
 
   public Customer toEntity(String encodingPassword){
