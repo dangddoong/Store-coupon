a299ae52ef10398cd1800d552b60827493b758f0
dangddoong <dangddoong@gmail.com>
2023-09-08 14:41:05 +0900

    Feat: Coupon Entity 추가

---
+++ src/main/java/com/make/storecoupon/coupon/entity/CoverageType.java
@@ -0,0 +1,6 @@
+package com.make.storecoupon.coupon.entity;
+
+public enum CoverageType {
+  ALL_PRODUCTS,
+  SPECIFIC_PRODUCT
+}
---
+++ src/main/java/com/make/storecoupon/coupon/entity/DiscountType.java
@@ -0,0 +1,6 @@
+package com.make.storecoupon.coupon.entity;
+
+public enum DiscountType {
+  FIXED_AMOUNT,
+  PERCENTAGE
+}
